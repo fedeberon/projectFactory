78e36f76e5d0be047889ed00421ebc29879ea671
Luciano D'Agata <martinvilla481@gmail.com>
2021-05-20 11:21:22 -0300

    Avances con NextJs getServerSideProps

--- pages/professional/index.js
+++ pages/professional/index.js
@@ -3,21 +3,24 @@ import { useTranslation } from "react-i18next";
 import Header from "../../components/Header";
 import { Container } from "reactstrap";
 import FormProfessional from "../../components/FormProfessional";
-import { useSession } from "next-auth/client";
+import { getSession, useSession } from "next-auth/client";
 import { getProfessionals, addProfessional } from "../_clientServices";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { findAll } from "../../services/professionalService";
 
-const Professional = () => {
+const Professional = ({ professionals }) => {
   const [session] = useSession();
   const [data, setData] = useState();
   const [isLoading, setLoading] = useState(false);
 
-  const { t, lang } = useTranslation("common"); 
+  const { t, lang } = useTranslation("common");
 
   const updateProfessionalList = async () => {
     setLoading(true);
-    const professionals = await getProfessionals();
-    setData(professionals);
+    // const professionals = await getProfessionals();
+    if (professionals) {
+      setData(professionals);
+    }
     setLoading(false);
   };
 
@@ -25,7 +28,7 @@ const Professional = () => {
     setLoading(true);
     await addProfessional(data, session);
     await updateProfessionalList();
-  }
+  };
 
   useEffect(async () => {
     await updateProfessionalList();
@@ -35,9 +38,7 @@ const Professional = () => {
     <Container fluid>
       <Header lang={lang} />
       <h1>{t("Professional")}</h1>
-      <FormProfessional
-        onAddProfessional={onAddProfessional}
-      />
+      <FormProfessional onAddProfessional={onAddProfessional} />
 
       {isLoading ? (
         <h1>{t("Loading")}...</h1>
@@ -46,9 +47,15 @@ const Professional = () => {
       ) : (
         data.map((project) => (
           <div key={project.id}>
-            <p>{t("Name")}: {project.firstName}</p>
-            <p>{t("Description")}: {project.lastName}</p>
-            <p>{t("Email")}: {project.email}</p>
+            <p>
+              {t("Name")}: {project.firstName}
+            </p>
+            <p>
+              {t("Description")}: {project.lastName}
+            </p>
+            <p>
+              {t("Email")}: {project.email}
+            </p>
           </div>
         ))
       )}
@@ -56,10 +63,33 @@ const Professional = () => {
   );
 };
 
-export const getStaticProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ["common"])),
-  },
-});
+export async function getServerSideProps({ params, req, res, locale }) {
+  // Get the user's session based on the request
+  const session = await getSession({ req });
+
+  let token = session.accessToken;
+  let professionals;
+  let { page, size } = req.__NEXT_INIT_QUERY;
+
+  if (!page || page <= 0) {
+    page = 0;
+  }
+  if (!size || size <= 0) {
+    size = process.env.NEXT_PUBLIC_SIZE_PER_PAGE;
+  }
+
+  if (session) {
+    token = session.accessToken;
+    professionals = await findAll(page, size, token);
+    console.log("FindAll: ", professionals);
+  }
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      professionals,
+    },
+  };
+}
 
 export default Professional;
