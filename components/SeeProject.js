e4f026c2e197deaa6b0eba0ab17c7f40d3de70cb
Tomas Arras <tomasarras@gmail.com>
2021-05-28 17:56:19 -0300

    add video in project

--- components/SeeProject.js
+++ components/SeeProject.js
@@ -72,7 +72,7 @@ const SeeProject = ({ project, onEditProject }) => {
         <div>
             <SeeProjectImages images={project.images}/>
         </div>
-        
+        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${project.videoPath}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </>
     );
 }
--- services/projectService.js
+++ services/projectService.js
@@ -1,4 +1,5 @@
 import API from "./api";
+import * as youtubeService from '../services/youtubeService';
 
 export const findAll = async (page, size, token) => {
   API.defaults.headers.common["Authorization"] = token;
@@ -17,11 +18,16 @@ export const getById = async (id, token) => {
 };
 
 export const addProject = async (project, token, id) => {
-  project.previewImage = null;
-  project.images = null;
-  project.files = null;
-  API.defaults.headers.common["Authorization"] = token;
-  return await API.post(`/projects?professional=${id}`, project);
+  delete project.previewImage;
+  delete project.images;
+  delete project.files;
+  try {
+    project.videoPath = youtubeService.getIdVideo(project.videoPath);
+    API.defaults.headers.common["Authorization"] = token;
+    return await API.post(`/projects?professional=${id}`, project);
+  } catch (err) {
+    throw new Error(`Error, link to video invalid: ${err}`);
+  }
 };
 
 export const addPreviewImage = async (image, projectId, token) => {
