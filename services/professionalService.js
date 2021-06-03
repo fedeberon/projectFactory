95c6d3921bc1d1dca6f12498d42b1d3719cffa7d
Tomas Arras <tomasarras@gmail.com>
2021-06-03 09:15:56 -0300

    change endpoints for upload and get images

--- services/professionalService.js
+++ services/professionalService.js
@@ -4,8 +4,8 @@ export const findAll = async (page, size, token) => {
   API.defaults.headers.common["Authorization"] = token;
   const professionals = await API.get(`/professionals?page=${page}&size=${size}`);
   professionals.forEach((professional) => {
-    professional.previewImage = `${process.env.NEXT_PUBLIC_HOST_BACKEND}/images/professionals/${professional.id}/${professional.previewImage}`;
-    professional.backgroundImage = `${process.env.NEXT_PUBLIC_HOST_BACKEND}/images/professionals/${professional.id}/${professional.backgroundImage}`;
+    professional.previewImage = `${process.env.NEXT_PUBLIC_HOST_BACKEND}/images/professionals/${professional.id}/preview/${professional.previewImage}`;
+    professional.backgroundImage = `${process.env.NEXT_PUBLIC_HOST_BACKEND}/images/professionals/${professional.id}/background/${professional.backgroundImage}`;
   });
   return professionals;
 };
@@ -26,14 +26,12 @@ export const addPreviewImage = async (image, professionalId, token) => {
   API.defaults.headers.common["Authorization"] = token;
   const imageData = new FormData();
   imageData.append('imageFile', image);
-  imageData.append('professional',professionalId);
-  return await API.post(`/images/professional/preview`, imageData);
+  return await API.post(`/images/professionals/${professionalId}/preview`, imageData);
 };
 
 export const addBackgroundImage = async (image, professionalId, token) => {
   API.defaults.headers.common["Authorization"] = token;
   const imageData = new FormData();
   imageData.append('imageFile', image);
-  imageData.append('professional',professionalId);
-  return await API.post(`/images/professional/background`, imageData);
+  return await API.post(`/images/professionals/${professionalId}/background`, imageData);
 };
