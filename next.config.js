b5e8f338d9f09df827166d07dd8bb9ed5e23129b
LucianoMDV <martinvilla481@gmail.com>
2021-08-26 16:12:13 -0300

    probando configuracion images

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextTranslate = require("next-translate");
 module.exports = {
   ...nextTranslate(),
   images: {
-    domains: ["localhost", "https://project-factory.vercel.app", "project-factory.vercel.app"],
+    domains: ["project-factory.vercel.app"],
     deviceSizes: [640, 750],
     imageSizes: [16, 32],
   },
