c21426a0a6fa7d3c2d6df1201b5ee7950a1478d2
LucianoMDV <martinvilla481@gmail.com>
2021-08-26 17:40:24 -0300

    ahora con suerte

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextTranslate = require("next-translate");
 module.exports = {
   ...nextTranslate(),
   images: {
-    domains: ["https://project-factory.vercel.app", "https://vps-2103012-x.dattaweb.com:8085/project-factory/api/v1"]
+    domains: [
+      "project-factory.vercel.app",
+      "https://project-factory.vercel.app",
+    ],
   },
 };
