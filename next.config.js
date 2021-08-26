453bb095a3dff956a20953902a5a5035800e6939
LucianoMDV <martinvilla481@gmail.com>
2021-08-26 17:15:38 -0300

    ultimo intento

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextTranslate = require("next-translate");
 module.exports = {
   ...nextTranslate(),
   images: {
-    domains: ["https://project-factory.vercel.app/portfolio"],
-    deviceSizes: [640, 750],
-    imageSizes: [16, 32],
+    domains: ["https://project-factory.vercel.app"]
   },
 };
