734704014627387acba1d1b69d9eec1b72247e78
LucianoMDV <martinvilla481@gmail.com>
2021-09-16 15:21:40 -0300

    Magazine en el home, no se ve los swiper ahora si

--- components/Magazine/Magazine.js
+++ components/Magazine/Magazine.js
@@ -3,38 +3,44 @@ import { Card } from "react-bootstrap";
 import Link from "next/link";
 import styles from "./Magazine.module.css";
 import PrimaryButton from "../Buttons/PrimaryButton/PrimaryButton";
+import useTranslation from "next-translate/useTranslation";
 
 const Magazine = (props) => {
-  const { magazine } = props;
+  const { magazine, description } = props;
+
+  const { t } = useTranslation("common");
+
   return (
     <Link
       href={`/magazine/${magazine.title.replace(/\s+/g, "-").toLowerCase()}-${
         magazine.id
       }`}
     >
-      <Card>
+      <div className={`${styles.magList} border-0`}>
         <div className={styles.divImg}>
           <label className={styles.label}>{magazine.category.name}</label>
           <img
             className={`${styles.img} cursor-pointer`}
             src={magazine.previewImage}
-            />
+          />
         </div>
-          
-        <Card.Body>
-          <Card.Title>{magazine.title}</Card.Title>
-          <Card.Text>{magazine.description}</Card.Text>
-        </Card.Body>
-        <Card.Footer className="bg-white">
+
+        <div className={`${styles.info}`}>
+          <h3 className={`${styles.name}`}>{magazine.title}</h3>
+          {description && (
+            <p className={`${styles.p}`}>{magazine.description}</p>
+          )}
+        </div>
+        <div>
           <Link
-            href={`/magazine/${magazine.title.replace(/\s+/g, "-").toLowerCase()}-${
-              magazine.id
-            }`}
+            href={`/magazine/${magazine.title
+              .replace(/\s+/g, "-")
+              .toLowerCase()}-${magazine.id}`}
           >
-            <PrimaryButton>Leer</PrimaryButton>
+            <PrimaryButton>{t("read")}</PrimaryButton>
           </Link>
-        </Card.Footer>
-      </Card>
+        </div>
+      </div>
     </Link>
   );
 };
