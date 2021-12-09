4ddf05c07014cad902be792d04e27fef27eca6bd
fedeberon <federicoberon@outlook.com>
2021-12-09 14:32:41 -0300

    Terminos y condiciones.

---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,139 @@
+const TermsAndConditions = () => {
+    return (
+        <div className={"container"}>
+
+            <p>TÉRMINOS Y CONDICIONES</p>
+            Aviso legal
+
+                <p>
+            Bienvenidos a la plataforma de e-Commerce de FABRICA DE PROYECTOS. Los siguientes Términos y Condiciones Generales (en adelante, los “Términos y Condiciones”) se aplicarán para la navegación del Sitio de Internet www.tiendagrundfos.com.ar (en adelante, el “Sitio”) y los servicios ofrecidos en él.
+
+            Aceptación y conocimiento de los Términos y Condiciones
+                </p>
+            Toda persona que acceda, utilice y navegue por el Sitio se encuentra obligada a cumplir y respetar los Términos y Condiciones. Se aplican exclusivamente a todas las compras y actividades realizadas mediante el Sitio, Si usted no está de acuerdo con los Términos y Condiciones, deberá abstenerse de utilizar el Sitio y/o los servicios por él ofrecidos. Por “Usuario” del Sitio se entiende a todas aquellas personas que utilicen el Sitio, estén o no registrados en el mismo.
+
+            A través del acceso, utilización y navegación en el Sitio se está contratando la utilización de los servicios prestados por FABRICA DE PROYECTOS, de acuerdo a las cláusulas dispuestas en los Términos y Condiciones, que constituye un contrato para los Usuarios y FABRICA DE PROYECTOS.
+
+    <p>
+
+
+            FABRICA DE PROYECTOS se reserva el derecho de efectuar en cualquier momento, modificaciones en las presentes condiciones de uso y privacidad y adaptarla a novedades legislativas, jurisprudenciales, así como a las prácticas del mercado, sin necesidad de requerir el consentimiento de los Usuarios, y resultando obligación del usuario visitar periódicamente dicho sitio a fin de verificar la versión más actual del presente Aviso Legal.
+
+            Para las transacciones en curso que hayan comenzado con anterioridad a dichas modificaciones, subsistirán las condiciones vigentes al momento de su concertación, a menos que las nuevas modificaciones introducidas fueran más convenientes para el Usuario.
+
+            FABRICA DE PROYECTOS pondrá un aviso en el Sitio alertando a los Usuarios sobre estos cambios, durante un tiempo razonable. Sin perjuicio de lo anterior, los Usuarios son responsables de leer estos Términos y Condiciones cada vez que ingresen al Sitio.
+
+
+    </p>
+
+
+            FABRICA DE PROYECTOS realiza sus mejores esfuerzos por mantener y mejorar la calidad de sus servicios, sin embargo, no garantiza el acceso y uso continuado e ininterrumpido de su Sitio.
+
+            FABRICA DE PROYECTOS se reserva el derecho de interrumpir, suspender o modificar en cualquier momento los servicios ofrecidos en el presente Sitio, ya sea en forma permanente o transitoria. No se requerirá la conformidad de los Usuarios, ni será necesario aviso previo alguno. Asimismo, FABRICA DE PROYECTOS no garantiza la funcionalidad ni el acceso o uso permanente del Sitio, que podría interrumpirse por cuestiones técnicas ajenas a FABRICA DE PROYECTOS.
+
+            El sistema puede eventualmente no estar disponible debido a dificultades técnicas o fallas de conectividad, o por cualquier otra circunstancia ajena a FABRICA DE PROYECTOS; en tales casos se procurará restablecerlo con la mayor celeridad posible sin que por ello pueda imputársele algún tipo de responsabilidad.
+
+            FABRICA DE PROYECTOS no garantiza la exactitud de la información y datos incluidos en el sitio, por lo que no será responsable por ningún error u omisión contenidos en su sitio web. Tampoco garantiza, la inexistencia de eventuales dificultades técnicas o fallas en los sistemas o en Internet y que el Sitio se encuentre libre de virus, gusanos o cualquier otro elemento que pueda llegar a dañar o alterar el normal funcionamiento de un ordenador. Es responsabilidad y obligación exclusiva del Usuario contar con los antivirus y herramientas adecuadas para detectar, desinfectar y/o prevenir cualquier tipo de elementos y/o posibles daños de esta naturaleza.
+
+            FABRICA DE PROYECTOS no se responsabiliza por ningún daño que pueda producirse en los equipos informáticos de los Usuarios o de terceros como consecuencia de la navegación del presente Sitio, ni por daños y/o perjuicios directos ni indirectos, ni mediatos ni inmediatos, que pudieran eventualmente ser ocasionados por tales causales.
+
+            Registración
+
+            <p>
+            Los Usuarios pueden navegar libremente por el Sitio. Para un correcto funcionamiento del sistema, es necesario que los Usuarios mantengan sus datos actualizados en el Sitio. FABRICA DE PROYECTOS podrá proceder a verificar la identidad del Usuario y/o de los datos consignados por éste.
+
+            FABRICA DE PROYECTOS no se responsabiliza por la veracidad o certeza de los datos provistos por los Usuarios. Asimismo, FABRICA DE PROYECTOS se reserva el derecho de suspender temporal o definitivamente a los Usuarios en caso de incumplimiento de los Términos y Condiciones, como así también de rechazar solicitudes y/o pedidos realizados al cargar datos y/0 subir imageenes.
+
+            Los usuarios declaran que no han proporcionado ni proporcionarán datos personales íntimos o cuya utilización debida o indebida pueda dar origen a discriminación o conlleve un riesgo grave para éste. En particular, el usuario se obliga a no proporcionar a FABRICA DE PROYECTOS ningún dato relativo a origen racial o étnico, estado de salud presente y futuro, información genética, creencias religiosas, filosóficas y morales, afiliación sindical, opiniones políticas o preferencias sexuales.
+
+            Los Usuarios accederán a su cuenta personal (la “Cuenta”) mediante un correo electrónico y una clave personal que deberán escoger. En caso de que estos datos sean olvidados por el Usuario, FABRICA DE PROYECTOS cuenta con un servicio de ayuda para recuperarlos. Para esto se deberá hacer “click” en la opción correspondiente e ingresar el e-mail que se proporcionó al momento de la registración. Al finalizar, el sistema le enviará de manera confidencial a esa cuenta un mail para resetear la contraseña.
+
+            Así como FABRICA DE PROYECTOS se compromete a mantener la confidencialidad de los datos aportados por los Usuarios para su registro, los Usuarios se comprometen a mantener la confidencialidad de su nombre y clave de acceso. De conformidad con lo expresado en el párrafo anterior, los Usuarios son responsables del uso que se haga de su clave y deberán tener presente que si otras personas tienen o pueden tener en el futuro acceso a la cuenta de e-mail consignada como propia en el formulario de registración, éstas también podrían solicitar su clave de acceso y nombre de Usuario. Es obligación exclusiva del Usuario tomar las medidas pertinentes para que esto no suceda. El Usuario se compromete a notificar inmediatamente y de manera fehaciente a FABRICA DE PROYECTOS cualquier uso no autorizado de su Cuenta de Usuario, y a mantenerlo indemne en el caso de que esto produzca algún daño a FABRICA DE PROYECTOS o a terceros.
+
+            Con excepción de aquellas que tengan lugar por culpa o negligencia exclusiva de FABRICA DE PROYECTOS, el Usuario será responsable por todas las operaciones efectuadas a través de su Cuenta, por sí y/o por terceros, pues el acceso a la misma está restringido al ingreso y uso de su Clave Secreta, de conocimiento exclusivo del Usuario. Se aclara que está prohibida la venta, cesión o transferencia de la Clave bajo ningún título.
+
+            Capacidad
+
+            Para utilizar los servicios del Sitio se requiere tener capacidad legal para contratar. No podrán acceder a los servicios quienes carezcan de ella, los que hayan sido suspendidos o inhabilitados, ni los menores de edad. (En la República Argentina tal capacidad se adquiere a los 18 años de edad), así como Usuarios del Sitio que hayan sido suspendidos temporalmente o inhabilitados definitivamente.
+
+            Los padres, tutores o responsables de los menores de edad o incapaces que utilicen el Sitio serán responsables por dicho uso, incluyendo cualquier cargo, facturación o daño que se derive de él.
+
+            En caso de actuar en representación de una persona jurídica (empresa), el Usuario debe contar con capacidad para contratar a nombre de tal entidad y de obligar a la misma en los términos de este documento. Si el Usuario no contare con tales facultades, o actuare en exceso de las mismas, se entenderá que se ha comprometido también a título personal.
+            </p>
+           <p>
+            Política de Privacidad de los datos personales suministrados por el Usuario. Seguridad y tratamiento
+           </p>
+
+            <p>
+
+            Para poder utilizar el Sitio de manera eficiente y segura, los Usuarios deberán aportar ciertos datos, entre ellos, su nombre y apellido, cuenta de e-mail, sin los cuales se tornaría imposible brindar los servicios. Por eso se requiere que éstos sean verdaderos y exactos. Los datos recabados por los formularios correspondientes serán incorporados a una base de la cual es responsable FABRICA DE PROYECTOS. La información personal que los Usuarios ingresan en el Sitio es totalmente confidencial y FABRICA DE PROYECTOS garantiza a sus Usuarios que utilizará los datos dentro de las pautas establecidas por la Ley 25.326 de Protección de los Datos Personales. No obstante lo anterior, el Usuario deberá tener en cuenta que Internet no es un medio inexpugnable en cuanto a su seguridad.
+
+            Conforme a dicha ley, el usuario proporcionará voluntariamente sus datos a FABRICA DE PROYECTOS ya sea por escrito, verbalmente, electrónicamente, mediante encuestas, o mediante el envió de archivos electrónicos.
+
+            Los Usuarios tienen el derecho de acceder a la información de su Cuenta, y podrán modificar los datos ingresados cuando lo deseen. Cualquier Usuario del Sitio tendrá derecho a solicitar y obtener información sobre los datos personales que FABRICA DE PROYECTOS tenga en su base, quedando FABRICA DE PROYECTOS obligada a proporcionar la información solicitada dentro de los diez días corridos de haber sido intimada fehacientemente. En caso de que los datos sean requeridos por la vía legal, administrativa o judicial correspondiente, FABRICA DE PROYECTOS se verá compelida a revelar los mismos a la autoridad solicitante. En la medida en que la legislación y normas de procedimiento lo permitan, FABRICA DE PROYECTOS informará a los Usuarios sobre estos requerimientos. Los Usuarios también podrán ejercer el derecho de rectificación, cuando los datos que se posean fueran incorrectos. Asimismo, los Usuarios podrán requerir en cualquier momento la baja de su solicitud y la eliminación de su Cuenta de la base de datos.
+
+            Cuando se ingresan datos y números correspondientes a tarjetas de crédito estos son encriptados, asegurando así que se mantengan en total confidencialidad y no puedan ser vistos por otras personas.
+
+            Por el sólo hecho de registrarse en el Sitio, los Usuarios aceptan que FABRICA DE PROYECTOS tiene derecho a comunicarse con ellos por vía postal, telefónica o electrónica y enviar información que FABRICA DE PROYECTOS considere, a su exclusivo criterio, que pueda ser de su interés, incluyendo publicidad e información sobre ofertas y promociones. En caso de que los Usuarios no deseen ser contactados con estos fines, podrán manifestárselo fehacientemente a FABRICA DE PROYECTOS, quien procederá a interrumpir este tipo de comunicaciones en el menor tiempo que le sea posible.
+
+            Veracidad de la Información suministrada
+
+            En caso de que la información o los datos suministrados por el Usuario no sean verdaderos, éste será responsable por los daños que este hecho pudiera ocasionar.
+
+            Sugerencias y comentarios
+
+            Todas las sugerencias y comentarios que realice el Usuario podrán ser tenidas en cuenta, implementadas o adaptadas por FABRICA DE PROYECTOS sin que ello genere derecho alguno a favor de tal Usuario.
+
+            Cookies
+
+           </p>
+            El Sitio puede utilizar un sistema de seguimiento mediante “cookies”, para que el acceso a la información, al pasar de página en página, se realice con mayor rapidez. También ayuda en algunos casos a identificar a los Usuarios, sin necesidad de solicitarles la clave de acceso una y otra vez.
+
+            Estas cookies son pequeños archivos que envía la página visitada y se alojan en el disco duro del ordenador, ocupando poco espacio.
+
+            Se hace saber a los Usuarios que utilizando las opciones de su navegador podrán limitar o restringir según su voluntad el alojamiento de estas “cookies”, aunque es desaconsejable restringirlas totalmente.
+
+            El sistema podrá recoger información sobre sus preferencias e intereses. En el caso de que esto ocurra, la información será utilizada exclusivamente con fines estadísticos para mejorar los servicios que se prestan en el Sitio. FABRICA DE PROYECTOS aplicará, en la mayor medida en que sea posible, procedimientos de disociación de la información de modo que los titulares de los datos sean inidentificables.
+
+
+
+            <p>
+            Derechos reservados. Propiedad Intelectual
+
+            Todos los derechos del presente Sitio están reservados y corresponden a FABRICA DE PROYECTOS
+
+            El contenido del presente Sitio, incluyendo, aunque no limitado al texto, logos, gráficos, y todo el diseño en general, así como su base de datos y software, es de propiedad de FABRICA DE PROYECTOS o tiene derecho a usarlo en virtud de licencias de uso otorgadas y se encuentra protegido por las legislación nacional e internacional vigente sobre propiedad intelectual.
+
+            Si el Usuario considera que en el Sitio se viola o atenta de algún modo contra derechos de propiedad intelectual de terceros deberá notificarlo a FABRICA DE PROYECTOS en la dirección indicada en los presentes Términos y Condiciones Generales, acompañando toda la información y documentación necesaria que respalde la mencionada consideración.
+
+            No está permitida ninguna acción o uso de dispositivo, software, u otro medio tendiente a interferir tanto en las actividades y operatoria de FABRICA DE PROYECTOS como en las ofertas, descripciones, cuentas o bases de datos de FABRICA DE PROYECTOS. Cualquier intromisión, tentativa o actividad violatoria o contraria a las leyes sobre derecho de propiedad intelectual y/o a las prohibiciones estipuladas en este contrato harán pasible a su responsable de las acciones legales pertinentes, y a las sanciones previstas por este acuerdo, así como lo hará responsable de indemnizar los daños ocasionados.
+
+
+
+            Notificaciones
+
+            Todas las notificaciones y/o comunicaciones que deban efectuarse por el uso del Sitio bajo estos Términos y Condiciones Generales, deberán realizarse por escrito:  al Usuario: mediante correo electrónico, a la cuenta de correo consignada por éste, o por carta documento, al domicilio declarado en el formulario de registración; a FABRICA DE PROYECTOS: a la cuenta de correo electrónico info@fabricadeproyectos.com.ar.
+
+            Asimismo, se considerarán eficaces las comunicaciones que consistan en avisos y mensajes insertos en el Sitio, o que se envíen durante la prestación de un servicio, que tengan por finalidad informar a los Usuarios sobre determinada circunstancia.
+
+
+
+            Avisos publicitarios y links desde y hacia el sitio
+
+            Cuando el Usuario hace “click” en avisos publicitarios o links de terceros e ingresa en otros sitios que no pertenecen a FABRICA DE PROYECTOS, estará sujeto a los términos y condiciones de dichos sitios. El Usuario deberá leer detenidamente sus políticas de acceso y uso.
+
+            Las referencias en el Sitio a nombres, marcas, productos o servicios de terceros, o vínculos de hipertexto a sitios de Internet o información de terceros se proveen únicamente como una comodidad para nuestros usuarios y de ninguna forma constituyen o implican respaldo, patrocinio o recomendación por www.fabricadeproyectos.com.ar respecto de la tercera parte, su información, productos o servicios. www.fabricadeproyectos.com.ar no es responsable de las prácticas o políticas de esos terceros, ni del contenido de los sitios de Internet de cualesquiera terceros y no hace ninguna declaración o promesa con respecto a productos o servicios de terceros, o respecto del contenido o la exactitud de cualquier material alojado en dichos sitios de terceros. Si usted decide navegar o seguir un vínculo hacia cualquiera de tales sitios de Internet de terceros, lo hace enteramente bajo su propio riesgo y responsabilidad.
+
+            Los hipervínculos o enlaces existentes en el Sitio www.fabricadeproyectos.com.ar, que conectan con páginas web de terceros, tienen por finalidad mejorar la experiencia de navegación del usuario por el Sitio, poniendo a su disposición herramientas, contenidos y servicios prestados por terceros ajenos a FABRICA DE PROYECTOS. Sin perjuicio de ello, FABRICA DE PROYECTOS no controla, no respalda ni garantiza la seguridad, calidad, licitud, veracidad e idoneidad de los servicios y contenidos ofrecidos o brindados por terceros en el Sitio, o a través del mismo cuando estos se encuentren fuera del Sitio. En ese orden no responderá por los daños que pudieren experimentar los Usuarios que adquirieren productos o utilizaren servicios de terceros. El acceso y utilización de páginas web enlazadas desde el sitio www.tiendagrundfos.com.ar será exclusiva responsabilidad del Usuario, quien deberá tomar todas las medidas de precaución necesarias de acuerdo al tipo de servicio, contenido al que acceda, o producto que adquiera.
+
+            FABRICA DE PROYECTOS no garantiza la legalidad, actualidad, calidad ni utilidad de los contenidos, operaciones e informaciones que se comuniquen, reproduzcan y/o realicen en sitios enlazados de terceros ni la ausencia de nocividad de tales contenidos o servicios, por lo que el Usuario exime de toda responsabilidad a FABRICA DE PROYECTOS por los contenidos incluidos en los referidos sitios o los servicios que en ellos se brindan o promocionan.
+
+            El establecimiento de cualquier "hipervínculo" o enlace, entre una página web ajena al Sitio y cualquier página de este último solo podrá realizarse con expresa autorización de FABRICA DE PROYECTOS. En ningún caso FABRICA DE PROYECTOS será responsable por los contenidos o manifestaciones existentes en las páginas web desde donde se establezcan los hipervínculos hacia el sitio de FABRICA DE PROYECTOS. El hecho que exista un hipervínculo entre una página web y el sitio de FABRICA DE PROYECTOS no implica que ésta tenga conocimiento de ello, o que mantenga relación alguna con los titulares de la página web desde donde se establece el enlace. FABRICA DE PROYECTOS se reserva el derecho a solicitar la remoción o eliminación de cualquier enlace desde una página web ajena al sitio, en cualquier momento, sin expresión de causa, y sin que sea necesario preaviso alguno. El responsable de la página web desde la cual se efectuare el enlace tendrá un plazo de 24hs. contados a partir del pedido de FABRICA DE PROYECTOS para proceder a la remoción o eliminación del mismo.
+            </p>
+
+
+        </div>
+    )
+}
+
+export default TermsAndConditions;
